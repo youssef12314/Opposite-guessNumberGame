8ff0e0ae6ec2f870a459b45988216e5ab10b332f
youssef12314 <112815657+youssef12314@users.noreply.github.com>
2024-02-06 15:43:28 +0100

    now the bot is algorithmic

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-window.addEventListener("load", start);
+
+let lowRange=1;
+let highRange=100;
+let currentGuess;
+
+
+function startGame(){
+  generateRandomNumber();
+}
+
 
 
 function generateRandomNumber(){
-
-    const randomNumber=(Math.floor(Math.random()*100)+1);
-
-    const resultElement = document.getElementById("result");
-      resultElement.textContent = `Is ${randomNumber} your number?`;
-
-      const generateButton = document.getElementById("generateButton");
-      generateButton.style.display = "none";
-
-  }
+  currentGuess=Math.floor((lowRange+highRange)/2);
+  const resultElement=document.getElementById("result");
+  resultElement.textContent=`Is ${currentGuess} your number?`;
+}
 
 
   function numberCorrect(){
     const correctelement=document.getElementById("correct");
     correctelement.textContent=`i knew that was your number. I was just trolling`
     const optionButtons = document.getElementById("optionButtons");
-      optionButtons.style.display = "none";
+    optionButtons.style.display = "none";
   }
   
   function hideOptionButtons() {
     const optionButtons = document.getElementById("optionButtons");
     optionButtons.style.display = "none";
   }
-  
 
 
+  function updateRange(isTooHigh){
+    if (isTooHigh && currentGuess > lowRange){
+      highRange=currentGuess-1;
+    } else if (!isTooHigh && currentGuess < highRange) {
+      lowRange=currentGuess+1;
+    } else {
+      const resultElement=document.getElementById("result");
+      resultElement.textContent='I cant go any higher or lower!!';
+      return;
+    }
+    generateRandomNumber();
+  }
 
+  function wayTooHigh() {
+    updateRange(true);
+  }
 
+function wayTooLow() {
+    updateRange(false);
+}
+
+window.addEventListener("load", startGame);
 
 
 
